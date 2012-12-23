4e4761e5750da3a6ec28b4be3dec73bb7233a67c
Terbiy <terbiyarchitect@gmail.com>
2012-12-24 00:27:52 +0400

    Deleting

--- js/signedRequest.js
+++
@@ -1,7 +0,0 @@
-var doIt = FB.getLoginStatus(function(response) {
-	if (response.status === 'connected') {
-		var signedRequest = response.authResponse.signedRequest;
-	}
-	var guy = document.getElementById('guy');
-	guy.innerHTML = signedRequest;
-});
