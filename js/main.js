a2ebd4ff8ad1e05d6bde065296104b6e9d8b433a
User Turancho-rulesby <turancho-rulesby@fe55.hc.ru>
2012-12-28 12:54:00 +0000

    Added full handlebars, View managemenT

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 ////////////
 // CONFIG //
 ////////////
-Parse.View = Parse.View.extend({ // change standart behavior of View.remove() to make it detach events
-	remove: function() {
+// NEW
+var positions = document.getElementsByClassName('position');
+var Views = {}; // Object with id's of elements for Views
+for (var i = 0; i < positions.length; i++) {
+	Views[positions[i].getAttribute('id')] = null;
+}
+Parse.View = Parse.View.extend({
+	render: function() {
+		if(this.model && !this.collection) this.$el.html(this.template(this.model.toJSON()));
+		else if(!this.model && this.collection) this.$el.html(this.template(this.collection.toJSON()));
+		else if(this.toRender) this.$el.html(this.template(this.toRender));
+		else this.$el.html(this.template());
+	},
+	initialize: function() {	
+		Views[this.el.getAttribute('id')] = this;
+		if(this.init) this.init();
+		if(this.model) this.model.on('change', this.render, this);
+		if(this.collection) this.collection.on('change', this.render, this);
+		if(this.collection) this.collection.on('add', this.render, this);
+		this.render();	
+	},
+	remove: function() { // change standart behavior of View.remove() to make it detach events
 		this.$el.empty().detach();
 		return this;
 	}
 });
 Parse.initialize("7NWULxIRFzuMWrQ6bX1O8mm357Nz7jfHEWXhPevn", "yQmxvt5eKgJfbSCePpBM040ZUMj3iNHiucWBlpas");
+
+
 function dateToString(date) {
 	var trimmedDate = date.getFullYear().toString();
 	trimmedDate += '-';
@@ -18,11 +40,30 @@ function dateToString(date) {
 	trimmedDate += date.toLocaleTimeString().substring(0,5);
 	return trimmedDate
 }
+
+function submitRule() {
+	var now = new Date();
+	var objectToPublish = {
+		rule1: $('input#rule1').val(),
+		rule2: $('input#rule2').val(),
+		rule3: $('input#rule3').val(),
+		author: $('input#author').val(),
+		author_url: 'jlksjad.com'
+	};
+	Parse.Cloud.run('addRule', objectToPublish, {
+		success: function(obj) {
+			$('.submission').find('input').val(''); // Clear inputs
+			console.log(obj);
+		},
+		error: function(error, obj) {
+			console.error(JSON.parse(error.message).message);
+		}
+	});
+}
 ////////////////
 // STRUCTURE  //
 ////////////////
 var RuleObject = Parse.Object.extend('Rule',{});
-
 var RuleCollection = Parse.Collection.extend({
  	model: RuleObject,
 });
@@ -32,17 +73,12 @@ var RuleCollection = Parse.Collection.extend({
 var RuleView = Parse.View.extend({
 	className: 'rule',
 	tagName: 'li',
-	template: Parse._.template($('#template-rule').html()),
+	template: Handlebars.compile($('#template-rule').html()),
 	events: {
 		'click .ratingChange' : 'ratingChange',
 	},
-	initialize: function() {
-		this.model.on('change', this.render, this);	
+	init: function() {
 		this.model.set('datetime', dateToString(this.model.createdAt));
-		this.render();
-	},
-	render: function(rating) {
-		this.$el.html(this.template(this.model.toJSON()));
 	},
 	ratingChange: function(e) {
         var increment = parseInt($(e.target).data('add-rating'));
@@ -59,101 +95,89 @@ var RuleView = Parse.View.extend({
 	}
 });
 var RuleCollectionView = Parse.View.extend({
-    el: $('#rulesList'),
-	initialize: function() {
-        if(!this.collection) this.collection = new RuleCollection();
-		this.collection.on('add', this.addOne, this);
-		this.render();
+	el: $('#rulesList'),
+	init: function() {
+		this.collection = new RuleCollection();
 	},
 	render: function() {
         this.$el.html("");
 		this.collection.forEach(this.addOne, this); 
 	},
 	addOne: function(ruleModel) {
-		var ruleView = new RuleView({
-			model: ruleModel
-		});
+		var ruleView = new RuleView({model: ruleModel});
 		this.$el.prepend(ruleView.$el.fadeIn());
 	}
 });
+
+
+
 var SubmitRuleView = Parse.View.extend({
-	source: $('#template-submitRule').html(),
+	el: $('#leftColumnFirstDiv'),
+	template: Handlebars.compile($('#template-submitRule').html()),
 	events: {
 		'click .publish' : 'submitRule'
 	},
-	render: function() {
-        var template = Handlebars.compile(this.source);
-        $('.firstLeft').html(template({user: "123"}));
-	},
-	initialize: function() {
-		this.render();
-	},
-	// Can't undestand, why it's here.
+	init: function() {
+		this.toRender = {
+        	isAuthorised: !!Parse.User.current(), 
+        	username: Parse.User.current() ?  Parse.User.current().get('author_name') : ""		
+		}
+	},	
 	submitRule : function() {
-		var now = new Date();
-		var objectToPublish = {
-			rule1: $('input#rule1').val(),
-			rule2: $('input#rule2').val(),
-			rule3: $('input#rule3').val(),
-			author: $('input#author').val(),
-			author_url: 'jlksjad.com'
-		};
-		Parse.Cloud.run('addRule', objectToPublish,
-		{
-			success: function(obj) {
-				$('.submission').find('input').val(''); // Clear inputs
-				console.log(obj);
-			},
-			error: function(error, obj) {
-				console.error(JSON.parse(error.message).message);
-				alert(JSON.parse(error.message).message);
-			}
-		});
+		console.log('clicked');
 	}
 });
+
+
 var NavBarView = Parse.View.extend({
 	el: $('#navBar'),
-	source: $('#template-navBar').html(),
-	initialize: function(el) {
-		this.render();
+	template: Handlebars.compile($('#template-navBar').html()),
+	events: {
+		'click #logout': 'logout',
+		'click #modal-login': 'modalLogin'
 	},
-	render: function() {
-		var template = Handlebars.compile(this.source);
-		this.$el.html(template());
+	init: function() {
+		this.toRender = { isAuthorised: !!Parse.User.current() };
+		if(Parse.User.current())
+			this.toRender.name = Parse.User.current().get('author_name')
+	},
+	modalLogin: function() {
+		$('#login-modal').modal('toggle');
+	},
+	logout: function() {
+		Parse.User.logOut();
+		re_render();
 	}
 });
 
-var RulesNav = Parse.View.extend({
-   source: $('#template-rulesNav').html(),
-   className: 'rulesNav clearfix',
-   initialize: function() {
-       this.render();
-   },
-   render: function() {
-   		var template = Handlebars.compile(this.source);
-		$('#rulesnav').html(template());
-   }
-});
-
-// Here I compile the way the welcoming page is shown.
 var LogInView = Parse.View.extend({
-    template: $('#template-logInView').html(),
-    initialize: function(){
-        this.render();
-    },
+    template: Handlebars.compile($('#template-logInView').html()),
+    el: $('#login-modal'),
     render: function() {
-        var source = Handlebars.compile(this.template);
-        $('#login-modal').html(source);
+		this.$el.html(this.template);
+    },
+	events: {
+		'click i.icon-facebook' : 'login',
+		'click i.icon-twitter' : 'login',
+		'click i.icon-vk' : 'login',
+		'click i.icon-gplus' : 'login'
+    },
+    login: function(e) {
+    	var iconClass = e.srcElement.getAttribute('class');
+    	console.log(iconClass.slice(iconClass.search('-')+1))
     }
 });
 
+var RulesNav = Parse.View.extend({
+	el: $('#rulesnav'),
+	template: Handlebars.compile($('#template-rulesNav').html()),
+	init: function() {
+
+	}
+});
+
 var AppView = Parse.View.extend({
-    initialize: function() {
-		this.render();
-	}, 
 	render: function() {
-		var self = this;
-        $('.rightColumn').prepend($('#template-gifLoader').html());
 		this.navBar = new NavBarView();
         this.rulesNav = new RulesNav();
         this.rulesView = new RuleCollectionView();
@@ -186,32 +210,7 @@ function queryRules(condition, userId) {
 	return query.collection();
 }
 
-Handlebars.registerHelper('submit_button', function() {
-    if (Parse.User.current()) {
-        return new Handlebars.SafeString("<a class=\"btn btn-large btn-warning publish\">ОПУБЛИКОВАТЬ</a>");	
-    }
-    else {
-        return new Handlebars.SafeString("<a class=\"btn btn-large btn-warning modal-login\">ВОЙТИ И ОПУБЛИКОВАТЬ</a>");
-    }
-});
 
-Handlebars.registerHelper('get_username', function() {
-    if (Parse.User.current()) {
-        return new Handlebars.SafeString(Parse.User.current().get('author_name'));	
-    }
-    else {
-        return "";
-    }
-});
-
-Handlebars.registerHelper('navbar_login', function() {
-    if (Parse.User.current()) {
-        return new Handlebars.SafeString("<li><a href=\"#\" id=\"logout\">Logout</a></li>");	
-    }
-    else {
-        return new Handlebars.SafeString("<li><a href=\"#\" id=\"modal-login\">Login</a></li>");
-    }
-});
 
 ////////////
 // ROUTER //
@@ -252,92 +251,63 @@ var Router = Parse.Router.extend({
 //////////////
 //  EVENTS  //
 //////////////
-function loginWith(provider, user)
-{
-	console.log(user);
-	var query = new Parse.Query('User');
-	query.equalTo(provider + "_id", user[provider + "_id"]);
-	query.find({
-		success: function(Users) {
-			console.log(Users);
-			if (!Users.length) {
-				var newUser = new Parse.User(user);
-				console.log(newUser);
-				newUser.signUp(null,
-				{
-					success: function(user) {
-						re_render();
-					},
-					error: function(user, error) {
-						console.log('Днище ' + error.message);
-					}
-				})
-			}
-			else {
-				Parse.User._saveCurrentUser(Users[0]);
-				re_render();
-			}
-		},
-		error: function(Users, errror) {
-			alert("Error: " + error.code + " " + error.message);
-		}
-	})
-}
-
-function linkWith(provider, id) {
-	if (Parse.User.current()) {
-		Parse.User.current.set(provider + "_id", id);
-	}
-	else {
-		alert("please login first");
-	}
-}
-
-function login (event) {
+function loginWith (event) {
 	$('#login-modal').modal('hide');
 	switch(event.data.type) {
 		case "facebook":
+			// Checking is the guy is logged or not.
 			// Login function
-			FB.login(function(response) {
-		        if (response.authResponse) {
-		            // connected
-		            // Creating new user on the server.
-				    FB.api('/me', function(response) {
-					    var newUser = {
-					    	username:		response.id,
-					    	password: 	'12345',
-					    	facebook_id: 	response.id,
-					    	author_name:	response.name,
-					    	// userpic: 		response.picture,
-					    	// email:  	'gaga@gaga.com',
-					    };
-					    loginWith("facebook", newUser);
-				    });
-		        }
-		        else {
-		            // cancelled
-		        }
-		    });
+			var loginFB = function() {
+				FB.login(function(response) {
+			        if (response.authResponse) {
+			            // connected
+			            // Creating new user on the server.
+					    FB.api('/me', function(response) {
+						    var newUser = new Parse.User({
+						    	url:		response.link,
+						    	username:	response.name,
+						    	email:  	'gaga@gaga.com',
+						    	password: 	'12345'
+						    });
+						    newUser.signUp(null,
+								{
+								success: function(user)	{
+								    app.submitRule.render();
+								    app.navBar.render();
+								    app.rulesNav.render();
+								},
+								error: function(user, error) {
+									// Show the error message somewhere and let the user try again.
+									alert("Error: " + error.code + " " + error.message);
+								}
+							});
+					    });
+			        }
+			        else {
+			            // cancelled
+			        }
+			    });
+
+			};
 		    // Checking function.
-			// FB.getLoginStatus(function(response) {
-			// 	if (response.status === 'connected') {
-			// 		// connected
-			// 		FB.logout(function(response) {
-			// 			console.log('Пользователь вышел как с сайта, так и с Facebook.')
-			// 		});
-			// 	} else if (response.status === 'not_authorized') {
-			// 		// not_authorized
-			// 		console.log('Вы не авторизированы!');
-			// 	} else {
-			// 	// not_logged_in
-			// 	console.log('Вам необходимо зарегистрироваться!');
-			// 	loginFB();
-			// 		// I've got here to work with login() function. I guess
-			// 		// that the FB.getLoginStatus is going to transfer to the
-			// 		// login() methof in the EVENTS. This is going to be the
-			// 		// checkup if the user is already registered.
-			// 	}
-			// });
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					// connected
+					console.log('This guy is connected');
+					alert('Да вы уже зашли, сударь!');
+				} else if (response.status === 'not_authorized') {
+					// not_authorized
+					console.log('Да вы хер с горы, сударь!');
+				} else {
+				// not_logged_in
+				console.log('Вам необходимо зарегистрироваться!');
+				loginFB();
+					// I've got here to work with login() function. I guess
+					// that the FB.getLoginStatus is going to transfer to the
+					// login() methof in the EVENTS. This is going to be the
+					// checkup if the user is already registered.
+				}
+			});
 			break;
 		case "twitter":
 			twttr.anywhere(function (T) {
@@ -345,55 +315,148 @@ function login (event) {
 				T.bind("authComplete", function (e, user) {
       				// triggered when auth completed successfully
       				console.log(user);
-      				var newUser = {
-      					username: user.idStr,
-      					password: user.profileBackgroundColor,
-      					twitter_id: user.id,
-      					author_name: user.name,
-      					userpic: user.profileImageUrl
-      				};
-					loginWith("twitter", newUser);     				
-      			});
+      				var userid = user.idStr;
+      				var name = user.name;
+      				var userPic = user.profileImageUrl;
+
+      				var qSearchTW = new Parse.Query('User');
+      				qSearchTW.equalTo("username", userid);
+      				qSearchTW.find({
+						success: function(User) {
+							// console.log(User.length);
+							if(!User.length) {
+								var newUser = new Parse.User();
+								newUser.set("username", userid);
+								newUser.set("password", "test");
+								newUser.set("twAuth", {
+									userid: userid,
+									name: name,
+								});
+								newUser.set("userPic", userPic);
+
+								newUser.signUp(null,
+								{
+								success: function(user)	{
+								    app.submitRule.render();
+								    re_render();
+								},
+								error: function(user, error) {
+									// Show the error message somewhere and let the user try again.
+									alert("Error: " + error.code + " " + error.message);
+								}
+								});
+							}
+							else {
+								Parse.User._saveCurrentUser(User[0]);
+								re_render();
+							}
+						},
+						error: function() {
+
+					  	}
+					});	
+    			});
 			});
-		break;
+			break;
 		case "vk":
 			VK.Auth.login(function(response) {
-			  	if (response.session) {
+			  if (response.session) {
 			    	var userid = response.session.mid;
 			    	console.log(response.session);
 
-      				var newUser = {
-      					username: response.session.mid,
-      					password: response.session.sig,
-      					vk_id: response.session.mid,
-      					author_name: response.session.user.first_name + " " + response.session.user.last_name,
-      					// userpic: user.profileImageUrl
-      				};
-      				loginWith("vk", newUser);
-				}
+					var qSearchVK = new Parse.Query('User');
+					qSearchVK.equalTo("username", userid);
+					qSearchVK.find({
+						success: function(User) {
+							// console.log(User.length);
+							if(!User.length) {
+								var newUser = new Parse.User();
+								newUser.set("username", userid);
+								newUser.set("password", "test");
+								newUser.set("vkAuth", {
+									userid: userid,
+									name: response.session.user.first_name + " " + response.session.user.last_name
+								});
+
+								newUser.signUp(null,
+								{
+								success: function(user)	{
+								    app.submitRule.render();
+								    re_render();
+								},
+								error: function(user, error) {
+									// Show the error message somewhere and let the user try again.
+									alert("Error: " + error.code + " " + error.message);
+								}
+								});
+							}
+							else {
+								Parse.User._saveCurrentUser(User[0]);
+								re_render();
+							}
+						},
+						error: function() {
+
+					  	}
+						});						
+						}
+					// });
+			  // });
+			  //  else {
+			  // 	}
+			// }
 		});
 		break;
 		case "gplus": 
 			gapi.client.load('plus', 'v1', function() { 
+				// alert("gplus sdk loaded");
 				var clientId = '923974640051-ht5uitgkdjvl4pmhf4qlm5o22qaad211.apps.googleusercontent.com';
 				var apiKey = 'AIzaSyAM24v0bbMXxesyKUTwoWDpkwfVUv-fcXo';
 				var scopes = 'https://www.googleapis.com/auth/plus.me';
 				gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, function(authResult){
+					// console.log(authResult);
 					var request = gapi.client.plus.people.get({
           				'userId': 'me'
             		});
       				request.execute(function(resp) {
-						console.log(resp);
+						// console.log(resp);
 						var userid = resp.id;
 						var name = resp.displayName;
 
-	      				var newUser = {
-	      					username: resp.id,
-	      					password: resp.etag,
-	      					gplus_id: resp.id,
-	      					author_name: resp.displayName,
-	      				};
-	      				loginWith("gplus", newUser);
+						var qSearchGplus = new Parse.Query('User');
+						qSearchGplus.equalTo("gplusID", userid);
+						qSearchGplus.find({
+							success: function(User) {
+								if(!User.length) {
+									var newUser = new Parse.User();
+									newUser.set("username", userid);
+									newUser.set("password", "test");
+									newUser.set("gplusAuth", {
+										userid: userid,
+										name: name
+									});
+									newUser.set("gplusID", userid);
+
+									newUser.signUp(null,
+									{
+									success: function(user)	{
+										re_render();
+									},
+									error: function(user, error) {
+										// Show the error message somewhere and let the user try again.
+										alert("Error: " + error.code + " " + error.message);
+									}
+									});
+								}
+								else {
+									Parse.User._saveCurrentUser(User[0]);
+									re_render();
+								}
+							},
+							error: function() {
+
+							}
+						})
         			});
 				}); 
 			});
@@ -416,17 +479,6 @@ function notify() {
 	alert("change auth");
 }
 
-$("body").on("click", "a#modal-login", function() {
-		$('#login-modal').modal('toggle');
-	});
-
-$("body").on("click", "i.icon-facebook", {type: "facebook"}, login);
-$("body").on("click", "i.icon-twitter", {type: "twitter"}, login);
-$("body").on("click", "i.icon-vk", {type: "vk"}, login);
-$("body").on("click", "i.icon-gplus", {type: "gplus"}, login);
-
-$("body").on("click", "a#logout", logout);
-
 //////////////
 // ON START //
 //////////////
@@ -436,25 +488,3 @@ $(function() {
    Parse.history.start(); 
 });
 
-// NEW
-// var positions = document.getElementsByClassName('position');
-// var Views = {}; // Object with id's of elements for Views
-// for (var i = 0; i < positions.length; i++) {
-// 	Views[positions[i].getAttribute('id')] = null;
-// }
-// Parse.View = Parse.View.extend({
-// 	render: function() {
-// 		this.$el.html(this.template(this.model.toJSON()));
-// 	},
-// 	initialize: function() {
-// 		this.render();
-// 		Views[this.el.getAttribute('id')] = this;
-// 		// this.model.on('change', this.render, this);
-// 		// this.collection.on('change', this.render, this);
-// 		// this.collection.on('add', this.render, this);
-// 	},
-// 	remove: function() { // change standart behavior of View.remove() to make it detach events
-// 		this.$el.empty().detach();
-// 		return this;
-// 	}
-// });
